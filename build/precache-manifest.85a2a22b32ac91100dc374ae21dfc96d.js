a4259a8aaff9cfbde4d9ec83b5413f7e13cb6c39
douglas@budadigital.com.br <douglas@budadigital.com.br>
2020-02-27 23:53:55 -0300

    Building
    Teste de Building Netlify

---
+++ build/precache-manifest.85a2a22b32ac91100dc374ae21dfc96d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3bfd4c7833852c4e038d09f7ab904f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02bc00dbd0c48fe66d06",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b480e17ef3815d3196ee",
+    "url": "/static/js/2.883e4172.chunk.js"
+  },
+  {
+    "revision": "2d4e50db9bebc1822939159016933eb5",
+    "url": "/static/js/2.883e4172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02bc00dbd0c48fe66d06",
+    "url": "/static/js/main.1034c03f.chunk.js"
+  },
+  {
+    "revision": "b9e0ef2e86eb4d16b327",
+    "url": "/static/js/runtime-main.2344c806.js"
+  },
+  {
+    "revision": "3ee938d08714813f253c95577f2c27c8",
+    "url": "/static/media/logo.3ee938d0.png"
+  }
+]);
